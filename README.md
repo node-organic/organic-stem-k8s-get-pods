b74deca3188b4ed91c43df8958ebbc41abed35f9
outbounder <obiwonn@gmail.com>
2019-07-18 23:22:41 +0300

    strict matching selector to app=cellName

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ a helper for organic-stem-skeleton 2.1 based cells for listing matching pods:
 
 ## prerequirements
 
-- kubectl with aligned context
+- `kubectl` with aligned context
 
 ## usage
 
--- index.js
+++ index.js
@@ -2,22 +2,11 @@ const findSkeletonRoot = require('organic-stem-skeleton-find-root')
 const path = require('path')
 const {exec} = require('child_process')
 
-const getDeploymentSelectorsForCell = function (cellInfo) {
-  let deployments = cellInfo.dna.deployment
-  if (!Array.isArray(deployments)) deployments = [deployments]
-
-  for (let i = 0; i < deployments.length; i++) {
-    if (deployments[i].kind === 'Deployment') {
-      return deployments[i].spec.selector.matchLabels
-    }
-  }
-}
-
 module.exports = async function ({cellName, namespace = 'default', log = false}) {
   const root = await findSkeletonRoot()
   const loadCellInfo = require(path.join(root, 'cells/node_modules/lib/load-cell-info'))
   const cellInfo = await loadCellInfo(cellName)
-  let matchLabelsSelector = getDeploymentSelectorsForCell(cellInfo) || {'app': cellName}
+  let matchLabelsSelector = {'app': cellName}
   const labels = []
   for (let key in matchLabelsSelector) {
     labels.push(`-l=${key}=${matchLabelsSelector[key]}`)
