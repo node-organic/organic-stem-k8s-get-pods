d91b4ecba53c3e12267292761d2224c7cb29bfda
outbounder <obiwonn@gmail.com>
2019-10-15 00:25:51 +0300

    improves implementation to support awaiting pod container ready

--- index.js
+++ index.js
@@ -2,23 +2,46 @@ const findSkeletonRoot = require('organic-stem-skeleton-find-root')
 const path = require('path')
 const {exec} = require('child_process')
 
-module.exports = async function ({cellName, namespace = 'default', log = false}) {
+const check = (labels, namespace, log, waitPods, resolve, reject, retryCount, maxRetries = 10) => {
+  let cmd = `kubectl get pods ${labels.join(' ')} --namespace ${namespace} --no-headers -o json`
+  if (log) console.info('get k8s pods run:', cmd)  
+  exec(cmd, function (err, stdout, stderr) {
+    if (err) return reject(err)
+    let response = JSON.parse(stdout)
+    let podNames = response.items.filter((v) => {
+      if (!waitPods) return true
+      let isReady = false
+      v.status.containerStatuses.forEach((containerStatus) => {
+        if (containerStatus.ready) {
+          isReady = true
+        }
+      })
+      return isReady
+    }).map((v) => {
+      return v.metadata.name
+    })
+    if (waitPods) {
+      if (podNames.length !== 0) return resolve(podNames)
+      if (retryCount > maxRetries) {
+        return reject(new Error('failed to lookup pods at ' + labels.join(' ') + ' within namespace ' + namespace))
+      }
+      console.log('pods not found, retry...', retryCount)
+      setTimeout(function () {
+        check(labels, namespace, log, waitPods, resolve, reject, retryCount++)
+      }, 1000)
+    } else {
+      resolve(podNames)
+    }
+  })
+}
+
+module.exports = async function ({cellName, namespace = 'default', 
+  log = false, waitPods = false, labelName = 'app', labels = []}) {
   const root = await findSkeletonRoot()
   const loadCellInfo = require(path.join(root, 'cells/node_modules/lib/load-cell-info'))
   const cellInfo = await loadCellInfo(cellName)
-  let matchLabelsSelector = {'app': cellName}
-  const labels = []
-  for (let key in matchLabelsSelector) {
-    labels.push(`-l=${key}=${matchLabelsSelector[key]}`)
-  }
+  const allLabels = [`-l=${labelName}=${cellName}`].concat(labels.map((v) => '-l=' + v))
   return new Promise((resolve, reject) => {
-    if (log) console.info('get pods matching', labels)
-    let cmd = `kubectl get pods ${labels.join(' ')} --namespace ${namespace} --no-headers -o name`
-    exec(cmd, function (err, stdout, stderr) {
-      if (err) return reject(err)
-      resolve(stdout.split('\n').map((value) => {
-        return value.replace('pod/', '')
-      }).filter(v => v))
-    })
+    check(allLabels, namespace, log, waitPods, resolve, reject, 0)
   })
 }
